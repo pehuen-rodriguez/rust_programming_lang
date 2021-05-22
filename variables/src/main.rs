8638ded34fed3f80d48fcc231f13aca2fe9e8122
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-22 20:37:55 -0300

    Const and mut

---
+++ variables/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    const MAX_POINTS : u32 = 100_000;
+    let mut x = 5;
+    println!("Te value of x is: {}", x);
+    x = 6;
+    println!("Te value of x is: {}", x);
+    println!("Te value of MAX_POINTS is: {}", MAX_POINTS);
+}
