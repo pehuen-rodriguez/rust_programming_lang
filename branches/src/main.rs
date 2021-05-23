b29625442019cbeb8e317ff7ac81c7225c3640e8
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-23 19:15:45 -0300

    If with no parens

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let number = 3;
+
+    if number > 5 {
+        println!("Number was bigger than 5");
+    } else {
+        println!("Number was bigger than 5");
+    }
 }
