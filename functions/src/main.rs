36d9b632066143a816c476852559092dd3651572
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-23 18:57:02 -0300

    Multiple parameters

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    another_function(5);
+    another_function(5, 9);
 }
 
-fn another_function(x: i32) {
-    println!("Another function with {}", x);
+fn another_function(x: i32, y: i32) {
+    println!("Another function with {}", x * y);
 }
