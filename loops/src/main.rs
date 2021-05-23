c3337d7b56a0ed8f4cccaa303ef4c80be7736ccb
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-23 20:44:21 -0300

    loop through array with while

--- loops/src/main.rs
+++ loops/src/main.rs
@@ -5,4 +5,12 @@ fn main() {
         index += 1;
         println!("{} Hello, world!", index);
     }
+
+    let silly_array = [10, 20, 30];
+    index = 0;
+
+    while index < 3 {
+        println!("Val in array was {}", silly_array[index]);
+        index += 1;
+    }
 }
