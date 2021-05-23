d86314f780108a8e1b9927e0fd76cfae05f65fed
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-23 18:51:25 -0300

    More data types

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    const MAX_POINTS : u32 = 100_000;
+    const MAX_POINTS: u32 = 100_000;
     let mut x = 5;
     println!("Te value of x is: {}", x);
     x = 6;
@@ -11,4 +11,47 @@ fn main() {
     let spaces = "   ";
     let spaces = spaces.len();
     println!("Number of spaces changed the value type: {}", spaces);
+    let guess: u32 = "42".parse().expect("Not a number!");
+    println!("Explicit type example: {}", guess);
+
+    // addition
+    let sum = 5 + 10;
+    // subtraction
+    let difference = 95.5 - 4.3;
+    // multiplication
+    let product = 4 * 30;
+    // division
+    let quotient = 56.7 / 32.2;
+    // remainder
+    let remainder = 43 % 5;
+    let heart_eyed_cat = '😻';
+
+    let tup: (i32, f64, u8) = (12_000, 14.4, 1);
+    let (x, y, z) = tup;
+
+    println!(
+        "Several values: {}, {}, {}, {}, {}, {}, {}, {}, {}, {}",
+        sum, difference, product, quotient, remainder, remainder, heart_eyed_cat, x, y, z
+    );
+    println!("And also: {}", tup.2);
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    println!("third position is {}", months[2]);
+    another_function();
+}
+
+fn another_function() {
+    println!("from another function");
 }
