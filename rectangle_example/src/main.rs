440bba61c8aa19299b54da307a9b55ae5eec2fb5
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-24 17:23:08 -0300

    Refactor as struct

--- rectangle_example/src/main.rs
+++ rectangle_example/src/main.rs
@@ -1,12 +1,17 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let rect = (100, 150);
+    let rect = Rectangle{width: 100, height: 150};
 
     println!(
         "The area of the rectangle equals {}",
-        calculate_area(rect)
+        calculate_area(&rect)
     );
 }
 
-fn calculate_area(dimensions: (u32, u32)) -> u32 {
-    dimensions.0 * dimensions.1
+fn calculate_area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
