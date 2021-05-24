24a0ede85e549b7bde0d2a0d21615e9cf07f518d
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-24 16:30:04 -0300

    Using structs

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        username: String::from("juanca"),
+        email: String::from("juanca@gmail.com"),
+        active: true,
+        sign_in_count: 1
+    };
+
+    let mut user2 = User {
+        username: String::from("marleen"),
+        email: String::from("marllen@gmail.com"),
+        active: true,
+        sign_in_count: 10
+    };
+
+    user2.username = user1.username;
+    user2.email = user1.email;
+    user2.active = user1.active;
+    user2.sign_in_count = user1.sign_in_count;
+}
+
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool
 }
