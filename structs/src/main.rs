aadc8943ce0bdd76467cbf02831e248ef43313ff
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-24 16:33:16 -0300

    Build from fn with defaults

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -3,25 +3,38 @@ fn main() {
         username: String::from("juanca"),
         email: String::from("juanca@gmail.com"),
         active: true,
-        sign_in_count: 1
+        sign_in_count: 1,
     };
 
     let mut user2 = User {
         username: String::from("marleen"),
         email: String::from("marllen@gmail.com"),
         active: true,
-        sign_in_count: 10
+        sign_in_count: 10,
     };
 
     user2.username = user1.username;
     user2.email = user1.email;
     user2.active = user1.active;
     user2.sign_in_count = user1.sign_in_count;
+
+    let user3 = build_struct(String::from("coroto"), String::from("coroto@gmail.com"));
+
+    println!("User3 build with username: {}", user3.username);
+}
+
+fn build_struct(username: String, email: String) -> User {
+    User {
+        username: username,
+        email: email,
+        active: true,
+        sign_in_count: 1,
+    }
 }
 
 struct User {
     username: String,
     email: String,
     sign_in_count: u64,
-    active: bool
+    active: bool,
 }
