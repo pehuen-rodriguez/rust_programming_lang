bbaa7ffa074a2bc67409d282e16bf76796426274
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-24 20:38:48 -0300

    Base match example

---
+++ match_examples/src/main.rs
@@ -0,0 +1,23 @@
+#[derive(Debug)]
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn main() {
+    println!("Value in cents: {:?}", value_in_cents(Coin::Penny));
+    println!("Value in cents: {:?}", value_in_cents(Coin::Nickel));
+    println!("Value in cents: {:?}", value_in_cents(Coin::Dime));
+    println!("Value in cents: {:?}", value_in_cents(Coin::Quarter));
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
