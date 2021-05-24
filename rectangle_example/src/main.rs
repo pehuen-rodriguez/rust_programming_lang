67845ef6f6606cc7cad37daf4331dee124b71f73
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-24 17:29:37 -0300

    Method sintax

--- rectangle_example/src/main.rs
+++ rectangle_example/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect = Rectangle {
         width: 100,
@@ -12,6 +18,7 @@ fn main() {
 
     println!("The area of the rectangle is: {}", calculate_area(&rect));
     println!("With #[derive(Debug)] we can see the rect: {:?}", rect);
+    println!("The area of the rectangle is: {}", rect.area());
 }
 
 fn calculate_area(rectangle: &Rectangle) -> u32 {
