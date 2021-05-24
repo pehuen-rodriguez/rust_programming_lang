e67777a15187e679078602c602b5b43fe478830d
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-23 21:23:15 -0300

    String are pointers to the heap

--- scope/src/main.rs
+++ scope/src/main.rs
@@ -3,11 +3,13 @@ fn main() {
         let s = "string";
         println!("String here was {}", s);
     }
-    const ES :&str = "string";
+    const ES: &str = "string";
     println!("String isn't valid ouside that scope. {}", ES);
 
     let mut another_string = String::from("like this one");
 
     another_string.push_str(" con algo más");
     println!("String type is allocated on the heap. {}", another_string);
+    let this_string = another_string;
+    println!("They both point to the same memory space. {}", this_string);
 }
