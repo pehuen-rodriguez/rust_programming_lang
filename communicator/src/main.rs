bda9059f5a47edcde6d3c57475b027aa79bbe8d4
Pehuen <pehuen.rodriguez@nextroll.com>
2021-05-25 17:48:14 -0300

    Bring test back

--- communicator/src/main.rs
+++ communicator/src/main.rs
@@ -3,3 +3,11 @@ extern crate communicator;
 fn main() {
     communicator::client::connect();
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
