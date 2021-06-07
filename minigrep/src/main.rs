67e283a32c323efd7c32e6a15d1aea4be4043a2d
Pehuen <pehuen.rodriguez@nextroll.com>
2021-06-06 23:42:57 -0300

    A certain point in the minigrep lib

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,47 @@
+use std::env;
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem processing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for: {}", config.query);
+    println!("In filename:   {}", config.filename);
+    if let Err(e) = run(config) {
+        println!("The app failed with error {}", e);
+        process::exit(1);
+    };
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)?;
+
+    println!("Found text:\n{}", contents);
+    Ok(())
+}
+
+struct Config {
+    filename: String,
+    query: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
 }
