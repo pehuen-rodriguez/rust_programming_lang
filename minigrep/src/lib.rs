3d8a541114c5595d2b14bdf5f442572c378e322b
Pehuen <pehuen.rodriguez@nextroll.com>
2021-06-06 23:56:25 -0300

    Code splitted and first test failed

---
+++ minigrep/src/lib.rs
@@ -0,0 +1,44 @@
+use std::error::Error;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let mut f = File::open(config.filename)?;
+    let mut contents = String::new();
+
+    f.read_to_string(&mut contents)?;
+
+    Ok(())
+}
+
+pub struct Config {
+    pub filename: String,
+    pub query: String,
+}
+
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(query, contents));
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,8 +1,9 @@
 use std::env;
-use std::error::Error;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
+
+extern crate minigrep;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,36 +13,8 @@ fn main() {
         process::exit(1);
     });
 
-    println!("Searching for: {}", config.query);
-    println!("In filename:   {}", config.filename);
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("The app failed with error {}", e);
         process::exit(1);
     };
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let mut f = File::open(config.filename)?;
-    let mut contents = String::new();
-
-    f.read_to_string(&mut contents)?;
-
-    println!("Found text:\n{}", contents);
-    Ok(())
-}
-
-struct Config {
-    filename: String,
-    query: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
-    }
 }
