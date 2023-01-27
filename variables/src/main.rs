ee18ba9168fef2bdddf281d148dc73c9ed7f317a
Pehuen <pehuen.rodriguez@nextroll.com>
2023-01-27 14:42:50 -0300

    Nothing and some format

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,12 +1,16 @@
 fn main() {
     const MAX_POINTS: u32 = 100_000;
+
     let mut x = 5;
     println!("Te value of x is: {}", x);
+
     x = 6;
     println!("Te value of x is: {}", x);
     println!("Te value of MAX_POINTS is: {}", MAX_POINTS);
+
     let shadow = 5;
     let shadow = shadow + 10;
+
     println!("Te value of the sadowed thing is: {}", shadow);
     let spaces = "   ";
     let spaces = spaces.len();
